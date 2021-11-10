4b0362c7d88d7b88c07b5e2cd5a6bcc4621887f9
mechenghn <mecheng.hn@gmail.com>
2021-11-10 21:14:16 +0800

    v1.5/PlotSingleTicket_NN
    Plot lich su mua/ ban 1 ma cho ca nuoc ngoai va tu doanh.
    
    syntax:
    arg1: plot; arg2: <ticket>
    
    neu khong co arg 2 thi plot du lieu thong ke today/week/month top 20.

--- daily_data_consolidator.py
+++ daily_data_consolidator.py
@@ -19,6 +19,8 @@
 #- collect foreign data
 #v1.4 
 # -plot historic data of tickers using 'plot' argument
+#v1.5
+# -plot single ticket buy/sell history 
 
 
 #Run format
@@ -63,7 +65,7 @@ def truncate(n, digit):
 	else:	
 		return int(n*pw)/pw
 
-def plotbarchart(tickers, ticker_att, chart_title):
+def plotbarchart(tickers, ticker_att, chart_title, retaintime = 3):
 	left = []
 	height = []
 	tick_label = []
@@ -108,7 +110,12 @@ def plotbarchart(tickers, ticker_att, chart_title):
 	#axes.set_ylim([ymin,ymax])
 
 	# function to show the plot
-	plt.show()
+	if (retaintime > 0):
+		plt.show(block=False)
+		plt.pause(retaintime)
+		plt.close()
+	else:
+		plt.show()
 
 
 def exploretradeticker(element):
@@ -176,6 +183,83 @@ def consolidatedata(data,outputfile):
 		print('Write data: ' + outputfile + '\n')	
 		return ret_tickers	
 
+def plottopbuysale(buy_tickers, depth, title, TTL = 0):
+	sorted_tks = sorted(buy_tickers, key=operator.attrgetter('NetValue'), reverse=True) 
+	plot_tk=[]
+	count=0
+	if (depth > len(sorted_tks)):
+		return
+	for tk in sorted_tks:
+		if(count < depth and tk.NetValue > 0):
+			plot_tk.append(tk)
+		if(count > (len(sorted_tks)-depth) and tk.NetValue < 0):
+			plot_tk.append(tk)
+		count = count + 1
+	plotbarchart(plot_tk,'value', title, TTL)
+
+def plottopbuy(buy_tickers, depth, title, TTL = 0):
+	sorted_tks = sorted(buy_tickers, key=operator.attrgetter('NetValue'), reverse=True) 
+	plot_tk=[]
+	count=0
+	if (depth > len(sorted_tks)):
+		return
+	for tk in sorted_tks:
+		if(count < depth and tk.NetValue > 0):
+			plot_tk.append(tk)
+			count = count + 1
+		
+	plotbarchart(plot_tk,'value', title, TTL)
+
+def plotuptodatedata(inputdata, NN = False):
+	f = open(inputdata)
+	d = json.load(f)
+	input_data = d["items"][0]
+
+	#1 day
+	data = input_data['today']	
+	file = 'dummy.csv'
+	if (NN == False):
+		buy_tickers=consolidatedata(data,file)
+		plottopbuysale(buy_tickers,10,'Value - TODAY',0)
+	else:
+		buy_tickers=consolidatedataNN(data,file)
+		plottopbuysale(buy_tickers,10,'Value NN - TODAY',0)
+	
+
+	#5day
+	data = input_data['oneWeek']	
+	file = 'dummy.csv'
+	if (NN== False):
+		buy_tickers=consolidatedata(data,file)
+		plottopbuysale(buy_tickers,10,'Value - 5 DAYS',0)
+	else:
+		buy_tickers=consolidatedataNN(data,file)
+		plottopbuysale(buy_tickers,10,'Value NN - 5 DAYS',0)
+	
+
+	#20 day data
+	data = input_data['oneMonth']	
+	file = 'dummy.csv'
+	if (NN == False):
+		buy_tickers=consolidatedata(data,file)
+		plottopbuysale(buy_tickers,20,'Top BUY Value - LAST 20 DAYS',0)
+	else:
+		buy_tickers=consolidatedataNN(data,file)
+		plottopbuysale(buy_tickers,20,'Top BUY Value NN- LAST 20 DAYS',0)
+	
+
+	# year to date data
+	data = input_data['yearToDate']	
+	if (NN == False):
+		buy_tickers=consolidatedata(data,file)
+		plottopbuy(buy_tickers,20,'Top BUY Value - YEAR TO DATE',0)
+	else:
+		buy_tickers=consolidatedataNN(data,file)
+		plottopbuy(buy_tickers,20,'Top BUY Value NN- YEAR TO DATE',0)
+	
+
+
+
 def processdailydata(inputdata, outputfile, NN = False):
 	f = open(inputdata)
 	d = json.load(f)
@@ -203,8 +287,10 @@ def processdailydata(inputdata, outputfile, NN = False):
 	file = outputfile + datetime.now().strftime("%Y%m%d") + '_today.csv'
 	if (NN==False):
 		buy_tickers=consolidatedata(data,file)
+		plottopbuysale(buy_tickers,10,'Value - TODAY',0)
 	else:
 		buy_tickers=consolidatedataNN(data,file)
+	
 	# 5 day data
 	data = input_data['oneWeek']	
 	file = outputfile + datetime.now().strftime("%Y%m%d") + '_oneweek.csv'
@@ -212,6 +298,8 @@ def processdailydata(inputdata, outputfile, NN = False):
 		buy_tickers=consolidatedata(data,file)
 	else:
 		buy_tickers=consolidatedataNN(data,file)
+	plottopbuysale(buy_tickers,7,'Value - WEEK',0)
+
     #20 day data
 	data = input_data['oneMonth']	
 	file = outputfile + datetime.now().strftime("%Y%m%d") + '_onemonth.csv'
@@ -219,6 +307,7 @@ def processdailydata(inputdata, outputfile, NN = False):
 		buy_tickers=consolidatedata(data,file)
 	else:
 		buy_tickers=consolidatedataNN(data,file)
+	plottopbuysale(buy_tickers,10,'Value - MONTH',0)
 
 	#Write tracker data
 	tracker_file = outputfile + VALUE_TRACKER + '_month.csv'
@@ -228,7 +317,7 @@ def processdailydata(inputdata, outputfile, NN = False):
 	writetrackingdata(tracker_file,buy_tickers,track_tic,'volume', header)
 	#plot bar chart
 	sorted_tks = sorted(plot_tk, key=operator.attrgetter('NetValue'), reverse=True) 
-	plotbarchart(sorted_tks,'value', outputfile + 'VALUE - LastMonth')
+	plotbarchart(sorted_tks,'value', outputfile + 'VALUE - TRACK LIST - MONTH')
 
 	# year to date data
 	data = input_data['yearToDate']	
@@ -350,7 +439,7 @@ def writetrackingdata(tracker_file, tickers,track_tic, datatype = 'value', heade
 			print('Write tracking data to ' + tracker_file)
 		return plot_tk
 
-def exploresingleticker(inputdata, ticker, buy_or_sell = True, timeRange = 'm'):
+def exploresingleticker(inputdata, ticker, buy_or_sell = True, timeRange = 'm', NN = False):
 	f = open(inputdata)
 	d = json.load(f)
 	input_data = d["items"][0]
@@ -369,11 +458,54 @@ def exploresingleticker(inputdata, ticker, buy_or_sell = True, timeRange = 'm'):
 		buy = data['sell']
 	tickers = []
 	for buy_element in buy:
-		tickers.append(exploretradeticker(buy_element))
+		if (NN == False):
+			tickers.append(exploretradeticker(buy_element))
+		else:
+			tickers.append(exploretradetickerNN(buy_element))
 	for tk in tickers:
 		if (tk.name == ticker):
 			return tk.NetValue;
 	return 0
+
+def plotsingletickerhistory(single_ticker):
+	print('Plot single ticker: ' + single_ticker)
+	#tudoanh
+	path = os.getcwd() + '/consolidated_data/*.json'
+	files = glob.glob(path)
+	sorted_by_mtime_ascending = sorted(files, key=lambda t: os.stat(t).st_mtime)
+	tickers = []
+	count = 0
+	for data_path in sorted_by_mtime_ascending:
+		tk_date = data_path[data_path.find('.json')-8:data_path.find('.json')]
+		if (count == 0):
+			tk_val = exploresingleticker(data_path,plot_single_tk,True,'y')
+		else:
+			tk_val = exploresingleticker(data_path,plot_single_tk,True,'d') 
+			cumulative_val = tickers[count-1].NetValue
+			tk_val = float(tk_val) + cumulative_val
+		count+=1
+		tickers.append(ticker_info(tk_date,0,0,0,float(tk_val)))
+	plotbarchart(tickers,'value',single_ticker + ' Tu doanh',-1)
+
+	#nn
+	path = os.getcwd() + '/consolidated_data_nn/*.json'
+	files = glob.glob(path)
+	sorted_by_mtime_ascending = sorted(files, key=lambda t: os.stat(t).st_mtime)
+	tickers = []
+	count = 0
+	for data_path in sorted_by_mtime_ascending:
+		tk_date = data_path[data_path.find('.json')-8:data_path.find('.json')]
+		if (count == 0):
+			tk_val = exploresingleticker(data_path,plot_single_tk,True,'y', True)
+		else:
+			tk_val = exploresingleticker(data_path,plot_single_tk,True,'d', True) 
+			cumulative_val = tickers[count-1].NetValue
+			tk_val = float(tk_val) + cumulative_val
+		count+=1
+		tickers.append(ticker_info(tk_date,0,0,0,float(tk_val)))
+	plotbarchart(tickers,'value',single_ticker + ' NN',-1)
+
+
 
 #####EXECUTION####
 arg_count=0
@@ -387,6 +519,8 @@ for arg in sys.argv:
 	if (arg_count == 2):
 		if (arg.isnumeric() and int(arg) < 30 and int(arg) > 5):
 			NO_OF_PLOTTING_TICKERS = int(arg)
+		elif (arg.upper() == 'ALL'):
+			plot_single_tk = 'XXX'
 		else:
 			plot_single_tk = arg.upper()
 	if (arg_count == 3):
@@ -447,47 +581,42 @@ if (op_mode=='plot'):
 
 	#looping through the data to find the tickers information
 	if (plot_single_tk!='XXX'):
-		path = os.getcwd() + '/consolidated_data/*.json'
-		files = glob.glob(path)
-		sorted_by_mtime_ascending = sorted(files, key=lambda t: os.stat(t).st_mtime)
-		tickers = []
-		for data_path in sorted_by_mtime_ascending:
-			tk_date = data_path[data_path.find('.json')-8:data_path.find('.json')]
-			tk_val = exploresingleticker(data_path,plot_single_tk,True,'y')
-			tickers.append(ticker_info(tk_date,0,0,0,float(tk_val)))
-		plotbarchart(tickers,'value',plot_single_tk)
+		plotsingletickerhistory(plot_single_tk)
+	else:
+		plotuptodatedata('input_data.json')
+		plotuptodatedata('input_data_NN.json', True)
 
 	#Read tracking data
-	tracker_file = DATA_FOLDER + VALUE_TRACKER + '.csv'
-	data = list(csv.reader(open(tracker_file)))
+#	tracker_file = DATA_FOLDER + VALUE_TRACKER + '.csv'
+#	data = list(csv.reader(open(tracker_file)))
  
 	#Plot year to date data
-	tickers=[]
-	for i in range(min(len(data[0])-1,NO_OF_PLOTTING_TICKERS)):
-		last_row_idx = len(data)-1
-		ticker = ticker_info(data[0][i+1],0,0,0,float(data[last_row_idx][i+1]))
-		tickers.append(ticker)
-		last_date = data[last_row_idx][0]
-	sorted_tickers = sorted(tickers, key=operator.attrgetter('NetValue'), reverse=False) 
-	plotbarchart(sorted_tickers,'value','Value Year To Date ' + last_date)
+#	tickers=[]
+#	for i in range(min(len(data[0])-1,NO_OF_PLOTTING_TICKERS)):
+#		last_row_idx = len(data)-1
+#		ticker = ticker_info(data[0][i+1],0,0,0,float(data[last_row_idx][i+1]))
+#		tickers.append(ticker)
+#		last_date = data[last_row_idx][0]
+#	sorted_tickers = sorted(tickers, key=operator.attrgetter('NetValue'), reverse=False) 
+#	plotbarchart(sorted_tickers,'value','Value Year To Date ' + last_date)
 
-	#plot each tiker data
-	for i in range(len(data[0])):
-		if (i>0 and i <= len(data[0])):
-			if (plot_single_tk == data[0][i]):
-				tk_title = str(data[0][i])
-				tickers = []
-				for j in range(len(data)):
-					if (j > 0):
-						tk_date = data[j][0]
-						if (i < len(data[j])):
-							tk_val = data[j][i]
-						else:
-							tk_val = 0.0
-						ticker = ticker_info(tk_date,0,0,0,float(tk_val))
-						tickers.append(ticker)
-				#plotbarchart(tickers,'value',tk_title)
-	#plot yeartodate data
+#	#plot each tiker data
+#	if (plot_single_tk == 'XXX'):
+#		for i in range(len(data[0])):
+#			if (i>0 and i <= len(data[0])):
+#				tk_title = str(data[0][i])
+#				tickers = []
+#				for j in range(len(data)):
+#					if (j > 0):
+#						tk_date = data[j][0]
+#						if (i < len(data[j])):
+#							tk_val = data[j][i]
+#						else:
+#							tk_val = 0.0
+#						ticker = ticker_info(tk_date,0,0,0,float(tk_val))
+#						tickers.append(ticker)
+#				plotbarchart(tickers,'value',tk_title)
+
 	
 
 
